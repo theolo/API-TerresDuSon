01c616d6f1c36314474a065a3c8313231ac41023
manar <manar.toshi@gmail.com>
2019-05-16 10:34:10 +0200

    Add product and product list

---
+++ produit/produit.php
@@ -0,0 +1,15 @@
+<?php
+
+class produit {
+    public $id, $nom, $valid, $email, $admin, $budget, $role, $date;
+    public function __construct($id, $nom, $password, $valid, $email, $admin, $budget, $role, $date) {
+        $this->id = $id;
+        $this->produit = $produit;
+        $this->prix = $prix;
+        $this->unit = $unit;
+        $this->fournisseur = $fournisseur;
+        $this->adresse = $adresse;
+       
+}
+
+?>
